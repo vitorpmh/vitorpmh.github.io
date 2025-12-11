6c2777a63788fb64e84680ccd77b48b1f15e5586
vitorpmh <82421942+vitorpmh@users.noreply.github.com>
2025-12-11 14:27:41 +0000

    Deploying to gh-pages from @ vitorpmh/vitorpmh.github.io@5dc3f7970f1550c5d2d34e6da673891c43b74c46 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,7 +501,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/vitor_matias_cv.pdf", "_blank");
         },
       },{
         id: 'social-email',
